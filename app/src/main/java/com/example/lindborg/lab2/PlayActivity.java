180f13e3da8b53454f317a2d5082d908d567b155
Patrik Lindborg <lindborg@Patriks-MacBook-Pro.local>
2018-02-13 15:30:50 +0100

    Fourth commit

--- app/src/main/java/com/example/lindborg/lab2/PlayActivity.java
+++ app/src/main/java/com/example/lindborg/lab2/PlayActivity.java
@@ -2,6 +2,10 @@ package com.example.lindborg.lab2;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PlayActivity extends Activity {
 
@@ -9,6 +13,20 @@ public class PlayActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        final Button playButton = findViewById(R.id.button5);
+        final EditText rightNumber = findViewById(R.id.editText);
+        final EditText leftNumber = findViewById(R.id.editText2);
+        final TextView resultView = findViewById(R.id.textView4);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Integer result = Integer.parseInt(leftNumber.getText().toString()) + Integer.parseInt(rightNumber.getText().toString());
+                //Integer result = 5 + 5;
+                resultView.setText(result.toString());
+            }
+        });
     }
 
 }
